f860296a628cdcfb1b0e0a3fdfe860c2e8c412f4
Daniele Gazzelloni <daniele@danielegazzelloni.com>
2018-10-02 14:37:35 +0200

    Added audio player

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -5,7 +5,7 @@ import React from 'react';
 const _AudioPlayer = () => (
 	<div className="player-container">
     <audio className="player" preload="true" controls="controls">
-    	{/*<source src="../../public/test-track.mp3" type="audio/mpeg" />*/}
+    	<source src="../../public/tracks/test-track.mp3" type="audio/mpeg" />
     	Your browser does not support HTML5 Audio! 
   	</audio>
   </div>
