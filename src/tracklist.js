0a7a10a54bdbffe3e2a2d69d50c5c339352578f0
Daniele Gazzelloni <daniele@danielegazzelloni.com>
2018-10-10 11:24:22 +0200

    Moved tracklist.js out of ./constants/ + removed "playing" field

--- src/tracklist.js
+++ src/tracklist.js
@@ -4,43 +4,37 @@ const tracklist = [
 		title: "Being Free",
 		author: "Audiojungle",
 		file: "audiojungle.mp3",
-		length: "3:18",
-		playing: true
+		length: "3:18"
 	},
 	{
 		title: "Fast Car",
 		author: "Jonas Blue (JUNI CLUB EDIT)",
 		file: "fast-car.mp3",
-		length: "4:09",
-		playing: false
+		length: "4:09"
 	},
 	{
 		title: "Going Higher",
 		author: "Ben Sound",
 		file: "bensound-goinghigher.mp3",
-		length: "4:05",
-		playing: false
+		length: "4:05"
 	},
 	{
 		title: "Slipstream",
 		author: "purple-planet.com",
 		file: "Slipstream.mp3",
-		length: "1:28",
-		playing: false
+		length: "1:28"
 	},
 	{
 		title: "Dubstep",
 		author: "Ben Sound",
 		file: "bensound-dubstep.mp3",
-		length: "2:05",
-		playing: false
+		length: "2:05"
 	},
 	{
 		title: "Chapati",
 		author: "Solartech",
 		file: "chapati-solartech.mp3",
-		length: "6:20",
-		playing: false
+		length: "6:20"
 	}
 ];
 
