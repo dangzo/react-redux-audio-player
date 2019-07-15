e5b199ec95a65a4caca1c654b592fe06e3c03b6a
Daniele Gazzelloni <daniele@danielegazzelloni.com>
2019-07-15 11:03:56 +0100

    Moved index.scss into styles folder. Moved <Provider> outside of App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,41 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
-import store from '../store';
 
 // App components
 import AudioPlayer from './AudioPlayer';
 
 const App = () => (
-  <Provider store={store}>
-    <Grid>
-      <Row>
-        <Col md={6} mdOffset={3}>
-          <PageHeader className="align-center">
-            React-Redux Audio Player
-          </PageHeader>
-        </Col>
-      </Row>
+  <Grid>
+    <Row>
+      <Col md={6} mdOffset={3}>
+        <PageHeader className="align-center">
+          React-Redux Audio Player
+        </PageHeader>
+      </Col>
+    </Row>
+    <Row className="showGrid">
+      <Col md={6} mdOffset={3}>
+        <AudioPlayer />
+      </Col>
+    </Row>
+    <footer>
       <Row className="showGrid">
         <Col md={6} mdOffset={3}>
-          <AudioPlayer />
+          <div className="align-center">
+            <p>Coded by Daniele Gazzelloni</p>
+            <p>
+              <a
+                href="https://github.com/dangzo/react-redux-audio-player"
+                target="_new"
+              >
+                https://github.com/dangzo/react-redux-audio-player
+              </a>
+            </p>
+          </div>
         </Col>
       </Row>
-      <footer>
-        <Row className="showGrid">
-          <Col md={6} mdOffset={3}>
-            <div className="align-center">
-              <p>Coded by Daniele Gazzelloni</p>
-              <p>
-                <a
-                  href="https://github.com/dangzo/react-redux-audio-player"
-                  target="_new"
-                >
-                  https://github.com/dangzo/react-redux-audio-player
-                </a>
-              </p>
-            </div>
-          </Col>
-        </Row>
-      </footer>
-    </Grid>
-  </Provider>
+    </footer>
+  </Grid>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import App from './components/App';
 
-import './index.scss';
+import './styles/index.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
